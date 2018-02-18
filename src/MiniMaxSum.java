14a44b287fc088455c1075ceb411fcd5fd7b1c5b
Skander Chouba <a.chouba957@gmail.com>
2018-02-18 14:30:36 +0100

    Mini-Max Sum Solution

--- src/MiniMaxSum.java
+++ src/MiniMaxSum.java
@@ -1,15 +1,28 @@
 import java.util.Scanner;
 
 public class MiniMaxSum {
-    static void miniMaxSum(int[] arr) {
+    static void miniMaxSum(long[] arr) {
+        long max = arr[0];
+        long min = arr[0];
+        long sum = 0;
         // Complete this function
+        for ( int i = 0; i <5; i++ ) {
+           sum += arr[i];
+           if ( arr[i] > max )
+               max = arr[i];
+           if ( arr[i] < min )
+               min = arr[i];
+
+        }
+
+        System.out.printf ( "%d %d", sum - max, sum - min );
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner (System.in);
-        int[] arr = new int[5];
+        long[] arr = new long[5];
         for(int arr_i = 0; arr_i < 5; arr_i++){
-            arr[arr_i] = in.nextInt();
+            arr[arr_i] = in.nextLong ();
         }
         miniMaxSum(arr);
         in.close();
