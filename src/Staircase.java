bc8259d4c089e839cea16ed28941de99aa48dd9c
Skander Chouba <a.chouba957@gmail.com>
2018-02-18 01:37:24 +0100

    Staircase

---
+++ src/Staircase.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Staircase {
+    static void staircase(int n) {
+        // Complete this function
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        int n = in.nextInt();
+        staircase(n);
+        in.close();
+    }
+}
