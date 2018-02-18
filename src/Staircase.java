9507a2ee0a0ba26f876e0b4dd06fdfd8e801b8a5
Skander Chouba <a.chouba957@gmail.com>
2018-02-18 02:09:45 +0100

    Staircase Solution

--- src/Staircase.java
+++ src/Staircase.java
@@ -2,6 +2,19 @@ import java.util.Scanner;
 
 public class Staircase {
     static void staircase(int n) {
+        for ( int i = 0; i < n; i++ ) {
+            for ( int j = 0; j <n - i - 1 ; j++ ) {
+                System.out.printf (" ");
+            }
+            for ( int j = n - i - 1; j < n; j++ ) {
+                System.out.printf ("#");
+            }
+
+            if ( i < n - 1 )
+                System.out.println ( "" );
+
+        }
+
         // Complete this function
     }
 
