a628d6126648eefa2d79dae9b69b8a32f0afadee
Skander Chouba <a.chouba957@gmail.com>
2018-02-18 14:47:34 +0100

    Time Conversion

---
+++ src/TimeConversion.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TimeConversion {
+    static String timeConversion(String s) {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        String s = in.next();
+        String result = timeConversion(s);
+        System.out.println(result);
+    }
+}
