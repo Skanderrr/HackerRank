ce50746fcb2839564d4979817906a86f676c2b2a
Skander Chouba <a.chouba957@gmail.com>
2018-02-18 01:34:52 +0100

    Plus Minus Solution

--- src/PlusMinus.java
+++ src/PlusMinus.java
@@ -3,7 +3,20 @@ import java.util.Scanner;
 public class PlusMinus {
     static void plusMinus(int[] arr) {
         // Complete this function
-
+        double plus = 0;
+        double minus = 0;
+        double zeros = 0;
+        for ( int i = 0; i < arr.length; i++ ){
+            if ( arr[i] > 0 )
+                plus += 1;
+            else if ( arr[i] < 0 )
+                minus += 1;
+            else
+                zeros += 1;
+        }
+        System.out.println ( plus / arr.length );
+        System.out.println ( minus / arr.length );
+        System.out.println ( zeros / arr.length );
     }
 
     public static void main(String[] args) {
