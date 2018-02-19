20887bbdff813788c5c48c2b0aa2e13d918e3d06
Skander Chouba <a.chouba957@gmail.com>
2018-02-19 11:32:16 +0100

    Time Conversion Solution

--- src/TimeConversion.java
+++ src/TimeConversion.java
@@ -2,7 +2,29 @@ import java.util.Scanner;
 
 public class TimeConversion {
     static String timeConversion(String s) {
+        String[] x = s.split ( ":" );
+        String y = "";
+        int xx = Integer.parseInt ( x[0] );
+        String ss = "";
+        if (s.contains ( "PM" )){
+            if (xx != 12) {
+                xx += 12;
+                x[0] = Integer.toString ( xx );
+            }
+        }
+        else if(s.contains ( "AM" ) && xx == 12)
+            x[0] = "00";
+        for (int i = 0; i< x.length; i++) {
+            if ( !x[i].contains ( "M" ) )
+                y += x[i] + ":";
+            else {
+                y += x[i].substring ( 0, 2 );
+            }
 
+            }
+
+
+        return y;
     }
 
     public static void main(String[] args) {
